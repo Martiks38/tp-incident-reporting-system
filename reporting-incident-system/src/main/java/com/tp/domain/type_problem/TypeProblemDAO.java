fdad05be32bc6102917bae0dc25505ecd89f23e9
Martiks <cappa.giuliano@gmail.com>
2023-12-03 19:33:22 -0300

    crear conexión persistence para TypeProblem

--- reporting-incident-system/src/main/java/com/tp/domain/type_problem/TypeProblemDAO.java
+++ reporting-incident-system/src/main/java/com/tp/domain/type_problem/TypeProblemDAO.java
@@ -3,13 +3,9 @@ package com.tp.domain.type_problem;
 import java.util.List;
 
 public interface TypeProblemDAO {
-  TypeProblem find(Long id);
+  TypeProblem findById(Long id);
+  
+  TypeProblem findByName(String name);
 
   List<TypeProblem> findAll();
-
-  void save(Long id, TypeProblem data);
-
-  void update(Long id, TypeProblem data);
-
-  void delete(Long id);
 }
---
+++ reporting-incident-system/src/main/java/com/tp/infrastructure/type_problem/PersistenceTypeProblem.java
@@ -0,0 +1,33 @@
+package com.tp.infrastructure.type_problem;
+
+import java.util.List;
+
+import com.tp.domain.type_problem.TypeProblem;
+import com.tp.domain.type_problem.TypeProblemDAO;
+
+import jakarta.persistence.EntityManager;
+
+public class PersistenceTypeProblem implements TypeProblemDAO {
+
+  private static EntityManager manager;
+
+  public PersistenceTypeProblem(EntityManager mg){
+    manager = mg;
+  }
+
+  @Override
+  public TypeProblem findById(Long id) {
+    return manager.find(TypeProblem.class, id);
+  }
+
+  @Override
+  public TypeProblem findByName(String name) {
+    return manager.find(TypeProblem.class, name);
+  }
+
+  @Override
+  public List<TypeProblem> findAll() {
+    return manager.createQuery("FROM TypeProblem", TypeProblem.class).getResultList();
+  }
+  
+}
