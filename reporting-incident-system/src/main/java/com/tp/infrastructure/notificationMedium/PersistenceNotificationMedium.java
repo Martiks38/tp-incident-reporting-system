33914ab99c15584f731bf11fa51c414b7de1174b
Martiks <cappa.giuliano@gmail.com>
2023-12-03 19:20:36 -0300

    crear conexión de persistencia de Technical y NotificationMedium

---
+++ reporting-incident-system/src/main/java/com/tp/infrastructure/notificationMedium/PersistenceNotificationMedium.java
@@ -0,0 +1,33 @@
+package com.tp.infrastructure.notificationMedium;
+
+import java.util.List;
+
+import com.tp.domain.notificationMedium.NotificationMedium;
+import com.tp.domain.notificationMedium.NotificationMediumDAO;
+
+import jakarta.persistence.EntityManager;
+
+public class PersistenceNotificationMedium implements NotificationMediumDAO{
+
+  private static EntityManager manager;
+
+  public PersistenceNotificationMedium(EntityManager mg){
+    manager = mg;
+  }
+
+  @Override
+  public NotificationMedium findById(Long id) {
+    return manager.find(NotificationMedium.class, id);
+  }
+
+  @Override
+  public NotificationMedium findByName(String name) {
+    return manager.find(NotificationMedium.class, name);
+  }
+
+  @Override
+  public List<NotificationMedium> findAll() {
+    return manager.createQuery("FROM NotificationMedium", NotificationMedium.class).getResultList();
+  }
+  
+}
---
+++ reporting-incident-system/src/main/java/com/tp/infrastructure/technical/PersistenceTechnicalDAO.java
@@ -0,0 +1,102 @@
+package com.tp.infrastructure.technical;
+
+import java.util.List;
+
+import com.tp.domain.technical.Technical;
+import com.tp.domain.technical.TechnicalCheckData;
+import com.tp.domain.technical.TechnicalDAO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class PersistenceTechnicalDAO implements TechnicalDAO{
+
+  private static EntityManager manager;
+
+  public PersistenceTechnicalDAO(EntityManager mg){
+    manager = mg;
+  }
+
+  @Override
+  public Technical findById(Long id) {
+    return manager.find(Technical.class, id);
+  }
+
+  @Override
+  public Technical findByName(String name){
+    return manager.find(Technical.class, name);
+  }
+
+  @Override
+  public List<Technical> findAll() {
+    return  manager.createQuery("FROM Technical", Technical.class).getResultList();
+  }
+
+  @Override
+  public void save(Technical data) {
+    EntityTransaction transaction = manager.getTransaction();
+
+    try {
+      transaction.begin();
+
+      TechnicalCheckData.check(data);
+
+      manager.persist(data);
+
+      transaction.commit();
+
+    } catch (Exception e) {
+      if(transaction != null && transaction.isActive()){
+        transaction.rollback();
+      }
+
+      // Para ver la traza pero se debería borrar y enviar la traza a un archivo o una base de datos que almacene los errores
+      e.printStackTrace();
+      System.err.println("Error en la transacción: " + e.getMessage());
+    }
+  }
+
+  @Override
+  public void update(Technical data) {
+    EntityTransaction transaction = manager.getTransaction();
+
+    try {
+      transaction.begin();
+
+      manager.merge(data);
+
+      transaction.commit();
+    } catch (Exception e) {
+      if(transaction != null && transaction.isActive()){
+        transaction.rollback();
+      }
+
+      // Para ver la traza pero se debería borrar y enviar la traza a un archivo o una base de datos que almacene los errores
+      e.printStackTrace();
+      System.err.println("Error en la transacción: " + e.getMessage());
+    }
+  }
+
+  @Override
+  public void delete(Long id) {
+    EntityTransaction transaction = manager.getTransaction();
+
+    try {
+      transaction.begin();
+
+      Technical t = manager.find(Technical.class, id);
+      
+      manager.remove(t);
+
+      transaction.commit();
+    } catch (Exception e) {
+      if(transaction != null && transaction.isActive()){
+        transaction.rollback();
+      }
+
+      // Para ver la traza pero se debería borrar y enviar la traza a un archivo o una base de datos que almacene los errores
+      e.printStackTrace();
+      System.err.println("Error en la transacción: " + e.getMessage());
+    }
+  }
+}
