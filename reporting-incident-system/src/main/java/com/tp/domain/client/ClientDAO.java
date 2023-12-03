28833ed591f32de01cb4e29c05488b4f6ba8ef9e
Juan Manuel <95452568+BiagioliJuanManuel@users.noreply.github.com>
2023-12-02 21:06:18 -0300

    ClientRepository
    metodos find, findAll, update, delete.

--- reporting-incident-system/src/main/java/com/tp/domain/client/ClientDAO.java
+++ reporting-incident-system/src/main/java/com/tp/domain/client/ClientDAO.java
@@ -3,13 +3,13 @@ package com.tp.domain.client;
 import java.util.List;
 
 public interface ClientDAO {
-  Client find(String id);
+  Client find(Long id);
 
   List<Client> findAll();
 
   void save(Client data);
 
-  void update(String id, Client data);
+  void update(Long id, Client data);
 
-  void delete(String id);
+  void delete(Long id);
 }
