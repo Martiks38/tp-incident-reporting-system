b2f0acdfe92e37c5d2a20f40c56f3bf7763732f5
Martiks <cappa.giuliano@gmail.com>
2023-12-04 19:38:15 -0300

    añadir prueba de método technicianWithMostIncidentsForNDaysBySpecialty a main

--- reporting-incident-system/src/main/java/com/tp/Main.java
+++ reporting-incident-system/src/main/java/com/tp/Main.java
@@ -5,9 +5,11 @@ import com.tp.domain.rrhh.Rrhh;
 public class Main {
     public static void main(String[] args) {
         final int days = 7;
+        final String specialty_name = "training";
 
         // Rrhh.generateReport();
         // Rrhh.technicianWithFasterIncidentResolution();
-        Rrhh.technicianWithMostIncidentsForNDays(days);
+        // Rrhh.technicianWithMostIncidentsForNDays(days);
+        Rrhh.technicianWithMostIncidentsForNDaysBySpecialty(days, specialty_name);
     }
 }
