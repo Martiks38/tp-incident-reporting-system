081273094947278daa6dc11768855ff788a0dbca
Martiks <cappa.giuliano@gmail.com>
2023-12-04 15:03:18 -0300

    crear singleton de EntityManager

---
+++ reporting-incident-system/src/main/java/com/tp/application/GetEntityManager.java
@@ -0,0 +1,28 @@
+package com.tp.application;
+
+import com.tp.assets.Constant;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public final class GetEntityManager {
+  final String persistenceUnitName = Constant.PERSISTENCE_UNIT_NAME;
+
+  private static GetEntityManager instance;
+  private static EntityManager manager;
+
+  private GetEntityManager() {
+    EntityManagerFactory factory = Persistence.createEntityManagerFactory(persistenceUnitName);
+
+    manager = factory.createEntityManager();
+  }
+
+  public static EntityManager getManager() {
+    if (instance == null) {
+      instance = new GetEntityManager();
+    }
+
+    return manager;
+  }
+}
