8c00a8d9e5264a4977a5e5cdf8688aee3f717500
Martiks <cappa.giuliano@gmail.com>
2023-12-06 16:19:01 -0300

    crear utilidad de fechas

---
+++ reporting-incident-system/src/main/java/com/tp/utils/GetDate.java
@@ -0,0 +1,23 @@
+package com.tp.utils;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import com.tp.assets.Constant;
+
+public class GetDate {
+  static final ZoneId GMTTimeZone = Constant.GMT_TIME_ZONE;
+
+  public static LocalDate calculateDateFromToday(Long numberDays) {
+    LocalDate currentDate = LocalDate.now();
+
+    if (numberDays == null) {
+      return currentDate;
+    }
+
+    return currentDate
+        .atStartOfDay(GMTTimeZone)
+        .plusDays(numberDays)
+        .toLocalDate();
+  }
+}
