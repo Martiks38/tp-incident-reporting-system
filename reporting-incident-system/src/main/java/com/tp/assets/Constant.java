5aafbd489d9e940f0db113d41f388e607bebc923
Martiks <cappa.giuliano@gmail.com>
2023-12-06 11:02:30 -0300

    añadir zona horaria de la app

--- reporting-incident-system/src/main/java/com/tp/assets/Constant.java
+++ reporting-incident-system/src/main/java/com/tp/assets/Constant.java
@@ -1,6 +1,9 @@
 package com.tp.assets;
 
+import java.time.ZoneId;
+
 public class Constant {
   public static final String PERSISTENCE_UNIT_NAME = "test-bd";
   public static final int LIMIT_CHARACTER_TEXT_CONSOLE = 80;
+  public static final ZoneId GMT_TIME_ZONE = ZoneId.of("GMT");
 }
