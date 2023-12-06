24bfe90454eea33bccce922a5dfddc5d1cdaff4b
Martiks <cappa.giuliano@gmail.com>
2023-12-06 11:02:05 -0300

    borrar CreateIncident

--- reporting-incident-system/src/main/java/com/tp/Main.java
+++ reporting-incident-system/src/main/java/com/tp/Main.java
@@ -1,28 +1,9 @@
 package com.tp;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import com.tp.application.GetEntityManager;
-import com.tp.application.Notification;
-import com.tp.application.notificationEmail;
-import com.tp.domain.client.Client;
-import com.tp.domain.incident.Incident;
-import com.tp.domain.notificationMedium.NotificationMedium;
 import com.tp.domain.operator.Operator;
 import com.tp.domain.rrhh.Rrhh;
 
-import com.tp.domain.technical.Technical;
-import com.tp.domain.type_problem.TypeProblem;
-import com.tp.infrastructure.client.PersistenceClient;
-import com.tp.infrastructure.incident.PersistenceIncident;
-import com.tp.infrastructure.technical.PersistenceTechnical;
-import com.tp.infrastructure.type_problem.PersistenceTypeProblem;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -83,55 +64,4 @@ public class Main {
 
     }
 
-    public static void CreateIncident() {
-        EntityManager manager = GetEntityManager.getManager();
-
-        EntityTransaction tx = manager.getTransaction();
-
-        try {
-            List<TypeProblem> listaProblemas = new ArrayList<>();
-            List<TypeProblem> listaProblemas2 = new ArrayList<>();
-
-            PersistenceTypeProblem TiposProblemas = new PersistenceTypeProblem(manager);
-            listaProblemas = TiposProblemas.findAll();
-
-            listaProblemas2.add(listaProblemas.get(1));
-            listaProblemas2.add(listaProblemas.get(2));
-
-            LocalDate today = LocalDate.now();
-            Date sqlDate = Date.valueOf(today);
-            PersistenceTechnical tecnicoRepo = new PersistenceTechnical(manager);
-            Technical tecnico = tecnicoRepo.findById(1L);
-            PersistenceClient clienteRepo = new PersistenceClient(manager);
-            Client cliente = clienteRepo.findById(1L);
-
-            PersistenceIncident pi = new PersistenceIncident(manager);
-            Incident primerIncidente = new Incident(false, "Problema de conexion", "No conecto el modem", sqlDate, null,
-                    true, tecnico, cliente, listaProblemas2);
-            pi.save(primerIncidente);
-            System.out.println(primerIncidente + "\n se ingreso correctamente");
-
-            NotificationMedium nm = tecnico.getMedium();
-            Notification notification = new Notification();
-
-            if (nm.getMedium().equals("Email")) {
-                notification.setStrategy(new notificationEmail());
-            }
-
-            if (nm.getMedium().equals("WhatsApp")) {
-                notification.setStrategy(new notificationEmail());
-            }
-
-            notification.executeStrategy("Se inicio un nuevo incidente" + primerIncidente);
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-
-            // Para ver la traza pero se debería borrar y enviar la traza a un archivo o una
-            // base de datos que almacene los errores
-            e.printStackTrace();
-            System.err.println("Error en la transacción: " + e.getMessage());
-        }
-    }
 }
