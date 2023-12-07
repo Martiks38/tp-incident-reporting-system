53d339f6c3fbbf53b8f46bdc7c56d5c412e798dd
Martiks <cappa.giuliano@gmail.com>
2023-12-07 20:11:51 -0300

    corregir nombre de archivo

--- reporting-incident-system/src/main/java/com/tp/application/notificationWhatsApp.java
+++ reporting-incident-system/src/main/java/com/tp/application/notificationWhatsApp.java
@@ -1,6 +1,7 @@
 package com.tp.application;
 
-public class notificationWhatsApp implements NotificationStrategy{
+public class NotificationWhatsApp implements NotificationStrategy {
+
     @Override
     public void send(String message) {
         System.out.println("WhatsApp: " + message);
