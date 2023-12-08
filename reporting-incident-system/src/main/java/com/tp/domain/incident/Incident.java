e25d6f158c346cd00c6aced6e0e7ed22e0694b66
Martiks <cappa.giuliano@gmail.com>
2023-12-08 13:41:20 -0300

    error comparar resolved cuando inicialmente es null

--- reporting-incident-system/src/main/java/com/tp/domain/incident/Incident.java
+++ reporting-incident-system/src/main/java/com/tp/domain/incident/Incident.java
@@ -65,13 +65,13 @@ public class Incident implements IncidentObservable {
   private Boolean resolved;
 
   public void setResolved(boolean resolved) {
+    if (this.resolved == null) {
+      this.resolved = resolved;
+    }
+
     if (this.resolved != resolved && resolved) {
       this.resolved = resolved;
       this.notifyObservable();
-    }
-
-    if (this.resolved == null) {
-      this.resolved = resolved;
     }
   }
 
